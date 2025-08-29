610af91d8acbc7cefdd50605e8b5e6a084c67376
Raunit2025 <raunitraj2336@gmail.com>
2025-08-29 15:11:18 +0530

    Error

--- project1/firstApp/urls.py
+++ project1/firstApp/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("id/<int:id>/", views.index, name="index"),
-    path("user/<str:username>/",views.user, name="user" ),
-    
-
+    path("id/<int:id>/<str:name>/", views.index, name="index"),
+    path("user/<str:username>/",views.user, name="user" )
 ]
--- project1/firstApp/views.py
+++ project1/firstApp/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request, id):
-    return HttpResponse(f"Hello, world. You're at the firstApp index with index value: {id}")
+def index(request, id, name):
+    return HttpResponse(f"Hello, {name} this is your id {id}")
 
 def user(request, username):
     return HttpResponse(f"The username which is passed through it {username}")
